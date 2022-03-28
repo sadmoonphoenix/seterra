22fa105a13385eb7bf796d849532e00dfa405a2a
sadmoonphoenix <64157802+sadmoonphoenix@users.noreply.github.com>
2022-03-28 15:39:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// Select the button
-const btn = document.querySelector('.btn-toggle');
-
-// Listen for a click on the button
-btn.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('dark-theme');  
-})
+// Listen for a click on the button 
+btn.addEventListener("click", function() {
+  // If the OS is set to dark mode...
+  if (prefersDarkScheme.matches) {
+    // ...then apply the .light-theme class to override those styles
+    document.body.classList.toggle("light-theme");
+    // Otherwise...
+  } else {
+    // ...apply the .dark-theme class to override the default light styles
+    document.body.classList.toggle("dark-theme");
+  }
+});
